17215f87d7a2204c9538470161d1a523321f4828
Yann Bayle <bayle.yann@live.fr>
2017-05-11 15:37:20 -0400

    Updated config for correct pip version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'satin',
   packages = ['satin'],
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'A Python package to manage the MIR dataset SATIN for research purposes',
   author = 'Yann Bayle',
   author_email = 'bayle.yann@live.fr',
   url = 'https://github.com/ybayle/SATIN',
-  download_url = 'https://github.com/ybayle/SATIN/archive/v1.0.0.tar.gz',
+  download_url = 'https://github.com/ybayle/SATIN/archive/v1.0.1.tar.gz',
   keywords = ['satin', 'MIR', 'dataset', 'research', 'reproducibility'],
   classifiers = [],
 )
